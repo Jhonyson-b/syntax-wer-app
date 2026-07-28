c758c609bad3354ff62d5283e19667a73b7ec174
Jhonyson Bastos <jhonysondev@hotmail.com>
2026-07-28 17:25:30 -0300

    feat: adiciona listagem de produtos

--- src/pages/_app/products/index.tsx
+++ src/pages/_app/products/index.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { ProductList } from '../../../components/ProductList'
+import { products } from '../../../mocks/products'
 
 export const Route = createFileRoute('/_app/products/')({
   component: RouteComponent,
@@ -6,6 +8,15 @@ export const Route = createFileRoute('/_app/products/')({
 
 function RouteComponent() {
   return (
-   <p>Hello Product</p>
+    <div className="container pt-40 md:pt-60 pb-10 md:px-10 mb-10 text-black bg-surface">
+      <h1 className="text-2xl md:text-3xl text-center mb-3">
+        Lista de produtos
+      </h1>
+
+      <h2 className="text-[14px] md:text-[16px] text-center mb-10">
+        Conforto excepcional para suas aventuras diárias.
+      </h2>
+      <ProductList products={products} />
+    </div>
   )
 }
