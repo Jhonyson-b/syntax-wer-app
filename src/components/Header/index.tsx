71481bb8222d411560678cee9f8a65d883e8fc73
Jhonyson Bastos <jhonysondev@hotmail.com>
2026-08-03 21:15:10 -0300

    feat: exclusão dos imports do header obsoletos git push

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,5 @@
 import Logo from '@/assets/images/logo.png'
 import IconUser from "@/assets/images/icon-user.png"
-import IconCart from "@/assets/images/icon-cart.png"
-import IconAbout from "@/assets/images/icon-about.png"
 import { Link } from '@tanstack/react-router'
 import { ShoppingCart } from '../ShoppingCart'
 import { MenuMobile } from '../MenuMobile'
